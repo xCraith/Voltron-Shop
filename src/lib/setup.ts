2110cd23b5e09618b776f17545c2bb30b1317ae9
xCraith <83143429+xCraith@users.noreply.github.com>
2024-09-15 00:08:48 +0200

    Update setup.ts

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -1,4 +1,4 @@
-// Unless explicitly defined, set NODE_ENV as development:
+
 process.env.NODE_ENV ??= 'development';
 
 import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framework';
@@ -8,11 +8,8 @@ import * as colorette from 'colorette';
 import { join } from 'node:path';
 import { srcDir } from './constants';
 
-// Set default behavior to bulk overwrite
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite);
 
-// Read env var
 setup({ path: join(srcDir, '.env') });
 
-// Enable colorette
 colorette.createColors({ useColor: true });
